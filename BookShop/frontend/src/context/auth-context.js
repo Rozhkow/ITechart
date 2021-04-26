2216f7084b9ac404725e179deed29d358110cb73
Dzmitry Razhkou <Dzmitry.Razhkou@itechart-group.com>
2021-04-26 17:03:54 +0300

    feat: upload app

--- BookShop/frontend/src/context/auth-context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// export this context to the highest lvl (app.js) of where we wanna use that because of using for children 
-export default React.createContext({
-    token: null,
-    userId: null,
-    login: (token, userId, tokenExpiration) => {},
-    logout: () => {}
-});
--- BookShop/frontend/src/pages/Login.js
+++ BookShop/frontend/src/pages/Login.js
@@ -3,29 +3,70 @@ import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-function LoginPage() {
+
+  function Login() {
+
+    const [values, setValues] = useState({
+      username: '',
+      password: '',
+    })
+  
+    const onChange = (event) => {
+      setValues({ ...values, [event.target.name]: event.target.value});
+    }
+    
+    const onSubmit = (event) => {
+      event.preventDefault(); 
+      login();
+    }
+
+    const [login, { loading }] = useMutation(LOGIN_USER, {
+      update(rezult){  // to update the cache after a mutation
+        console.log(rezult)
+      },
+      variables: values
+    });
+  
+
     return (
-    <div className="form-container">
-      <Form>
-        <h1>Login</h1>
-        <Form.Input
-          label="Username"
-          placeholder="Username.."
-          name="username"
-          type="text"
-        />
-        <Form.Input
-          label="Password"
-          placeholder="Password.."
-          name="password"
-          type="password"
-        />
-        <Button type="submit" primary>
-          Login
-        </Button>
-      </Form>
-    </div>
-    )
+      <div className="form-container">
+        <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
+          <h1>Login</h1>
+          <Form.Input
+            label="Username"
+            placeholder="Username.."
+            name="username"
+            type="text"
+            value={values.username}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Password"
+            placeholder="Password.."
+            name="password"
+            type="password"
+            value={values.password}
+            onChange={onChange}
+          />
+          <Button type="submit" primary>
+            Login
+          </Button>
+        </Form>
+      </div>
+    );
 }
 
-export default LoginPage;
+
+const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
+
+export default Login;
